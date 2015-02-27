b80348dacb641804143e804e5019b527a432a59d
atcg <evanmelstad@ucla.edu>
2015-02-26 21:34:38 -0800

    Added target-level metrics testing from depth files

---
+++ analyzeManderCap.R
@@ -0,0 +1,88 @@
+setwd("~/src/manderCap/")
+mappingStats <- read.csv("mappingStats.csv", stringsAsFactors=FALSE)
+mappingStatsNoDSN <- mappingStats[-c(18,21),]
+
+
+
+
+# Generate the nested models
+cot1Mod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$cot.multiplier)
+inputDNAmod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$individual_dna_in_capture)
+individualMod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$individual)
+cot1AndInputDNAmod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$cot.multiplier + mappingStatsNoDSN$individual_dna_in_capture)
+cot1AndIndividualMod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$cot.multiplier + mappingStatsNoDSN$individual)
+inputDNAandIndividualMod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$individual_dna_in_capture + mappingStatsNoDSN$individual)
+all3Mod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$individual_dna_in_capture + mappingStatsNoDSN$cot.multiplier + mappingStatsNoDSN$individual)
+
+
+cot1AndInputDNAmod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$cot.multiplier + mappingStatsNoDSN$individual_dna_in_capture + G(mappingStatsNoDSN$individual))
+
+
+
+totalDNAmod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$total_dna_in_capture)
+all4Mod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$individual_dna_in_capture + mappingStatsNoDSN$cot.multiplier + mappingStatsNoDSN$individual + mappingStatsNoDSN$total_dna_in_capture)
+all3v2Mod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$individual_dna_in_capture + mappingStatsNoDSN$cot.multiplier + mappingStatsNoDSN$total_dna_in_capture) 
+
+
+
+# Are CTS/BTS/F1 distinguishable?
+plot(mappingStatsNoDSN$condition, mappingStatsNoDSN$CTSonly6iterDiscountMapRate, col=mappingStatsNoDSN$individual, pch=20, ylab="Discounted capture rate", xlab="Reaction condition number", main="CTS-only assembly after six ARC iterations--looking for differences between CTS/BTS/F1")
+
+
+# Looks at predictive power of our variables
+plot(mappingStatsNoDSN$individual_dna_in_capture, mappingStatsNoDSN$CTSonly6iterDiscountMapRate, col=mappingStatsNoDSN$individual, pch=20, cex=1, ylab="Discounted capture rate", xlab="Individual DNA in capture", main="CTS-only assembly after six ARC iterations")
+plot(mappingStatsNoDSN$individual_dna_in_capture, mappingStatsNoDSN$CTSonly6iterRawMapRate, col=mappingStatsNoDSN$individual, pch=20, cex=1, ylab="Discounted capture rate", xlab="Individual DNA in capture", main="RAW MAP")
+plot(mappingStatsNoDSN$individual_dna_in_capture, mappingStatsNoDSN$CTSonly6iterPercDupes, col=mappingStatsNoDSN$individual, pch=20, cex=1, ylab="Discounted capture rate", xlab="Individual DNA in capture", main="CTS-only assembly after six ARC iterations")
+
+
+indDNAmod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$individual_dna_in_capture)
+summary(indDNAmod)
+abline(indDNAmod, col="red", lwd=2)
+indDNAmodResiduals <- resid(indDNAmod)
+indDNAmodInd<- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$individual_dna_in_capture + mappingStatsNoDSN$individual)
+summary(indDNAmodInd)
+
+
+all3mod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$individual_dna_in_capture + mappingStatsNoDSN$cot.multiplier + mappingStatsNoDSN$individual)
+summary(all3mod)
+anova(all3mod)
+
+bothMod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$individual_dna_in_capture + mappingStatsNoDSN$cot.multiplier)
+plot(bothMod)
+plot(fitted(bothMod), mappingStatsNoDSN$CTSonly6iterDiscountMapRate)
+abline(coef=c(0,1), col="red", lwd=2)
+
+# Plot of the residuals from the individual DNA model
+plot(mappingStatsNoDSN$individual_dna_in_capture, indDNAmodResiduals, col=mappingStatsNoDSN$individual, pch=20, cex=1, ylab="Residual", xlab="Individual DNA in capture", main="CTS-only assembly after six ARC iterations--residuals from individual input DNA model")
+
+
+
+
+# cot-1 Analyses
+plot(mappingStatsNoDSN$cot.multiplier, mappingStatsNoDSN$CTSonly6iterDiscountMapRate, col=mappingStatsNoDSN$individual, pch=20, cex=1, ylab="Discounted capture rate", xlab="cot-1 multiplier", main="CTS-only assembly after six ARC iterations")
+cot1mod <- lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$cot.multiplier)
+summary(cot1mod)
+abline(cot1mod, col="red", lwd=2)
+cot1modResiduals <- resid(cot1mod)
+
+# Plot of the residuals from the cot-1 model
+plot(mappingStatsNoDSN$individual_dna_in_capture, cot1modResiduals, col=mappingStatsNoDSN$individual, pch=20, cex=1, ylab="Residual", xlab="Individual DNA in capture", main="CTS-only assembly after six ARC iterations--residuals from cot-1 model")
+
+
+
+# Predict individual DNA residuals based on cot-1
+plot(mappingStatsNoDSN$cot.multiplier, indDNAmodResiduals, col=mappingStatsNoDSN$individual, pch=20)
+summary(lm(indDNAmodResiduals ~ mappingStatsNoDSN$cot.multiplier))
+abline(lm(indDNAmodResiduals ~ mappingStatsNoDSN$cot.multiplier), col="red", lwd=2)
+
+# Predict cot-1 residuals based on input DNA
+plot(mappingStatsNoDSN$individual_dna_in_capture, cot1modResiduals, col=mappingStatsNoDSN$individual, pch=20)
+summary(lm(cot1modResiduals ~ mappingStatsNoDSN$individual_dna_in_capture))
+abline(lm(cot1modResiduals ~ mappingStatsNoDSN$individual_dna_in_capture), col="red", lwd=2)
+
+
+
+
+summary(lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$individual_dna_in_capture + mappingStatsNoDSN$cot.multiplier))
+summary(lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$cot.multiplier))
+summary(lm(mappingStatsNoDSN$CTSonly6iterDiscountMapRate ~ mappingStatsNoDSN$individual_dna_in_capture))
---
+++ countSuccessfulTargets.pl
@@ -0,0 +1,285 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Getopt::Long;
+use Bio::SearchIO;
+use Bio::SeqIO;
+use Data::Dumper;
+use List::Util qw(sum);
+
+my $help = 0;
+my $blastFile;
+my $assembly;
+my $outFilePrefix;
+my $depthDir;
+
+GetOptions  ("blast=s"        => \$blastFile,
+             "assembly=s"     => \$assembly,
+             "depthfiledir=s" => \$depthDir,
+             "out=s"          => \$outFilePrefix,
+             "help|man"       => \$help) || die "Couldn't get options with GetOpt::Long: $!\n";
+
+if (!$blastFile or !$assembly or !$depthDir or !$outFilePrefix  or $help) {
+    die "Must supply --blast <blastoutput> --depthfiledir </path/to/dir/> --assembly <fasta> --out <filePrefix>.\n The blast file should be a blast output file\
+    in tabular format (-outfmt 6) where the target sequences have been blasted against\
+    the chimera-masked RBBH sequences\n";
+}
+
+# Here's what the blast file should look like
+#contig203242|E15C10|OPA allCTS_:_contig203242|E15C10|OPA_:_Contig001_contig203242|E15C10|OPA    98.20   389     7       0       1       389     499     887     0.0       680
+#contig189275|E15E12|OPA allCTS_:_contig189275|E15E12|OPA_:_Contig001_contig189275|E15E12|OPA    96.72   366     9       1       1       366     516     878     9e-174    606
+#contig42365|E15E3|OPA   allCTS_:_contig42365|E15E3|OPA_:_Contig001_contig42365|E15E3|OPA        95.48   398     9       2       4       393     555     951     7e-180    627
+
+
+# First, we want to create a list of the regions where the targets overlap with the
+# assembled contig that represents it as the reciprocal best blast hit. We'll put
+# these into a hash:
+my %targetMatchingRegions;
+
+
+
+
+my @samples = (
+               "01A_0801_CTS",
+               "01A_0809_F1",
+               "01A_0905_BTS",
+               "01B_0802_CTS",
+               "01B_0810_F1",
+               "01B_0906_BTS",
+               "01C_0803_CTS",
+               "01C_0811_F1",
+               "01C_0907_BTS",
+               "01D_0804_CTS",
+               "01D_0812_F1",
+               "01D_0908_BTS",
+               "01E_0805_CTS",
+               "01E_0901_BTS",
+               "01E_0909_BTS",
+               "01F_0806_CTS",
+               "01F_0902_BTS",
+               "01F_0910_DSN_BTS",
+               "01G_0807_F1",
+               "01G_0903_BTS",
+               "01G_0911_DSN_CTS",
+               "01H_0808_F1",
+               "01H_0904_BTS",
+               "02A_0801_F1",
+               "02A_0905_BTS",
+               "02B_0802_F1",
+               "02B_0906_CTS",
+               "02C_0803_F1",
+               "02C_0907_CTS",
+               "02D_0804_F1",
+               "02D_0908_CTS",
+               "02E_0805_F1",
+               "02E_0909_CTS",
+               "02F_0806_BTS",
+               "02F_0910_CTS",
+               "02G_0807_BTS",
+               "02G_0911_CTS",
+               "02H_0808_BTS",
+               "02H_0912_F1",
+               "03A_0809_BTS",
+               "03A_0905_CTS",
+               "03B_0810_BTS",
+               "03B_0906_F1",
+               "03C_0811_BTS",
+               "03C_0907_F1",
+               "03D_0812_CTS",
+               "03D_0908_F1",
+               "03E_0901_CTS",
+               "03E_0909_F1",
+               "03F_0902_CTS",
+               "03F_0910_F1",
+               "03G_0903_CTS",
+               "03G_0911_F1",
+               "03H_0904_CTS",
+               "03H_0912_BTS");
+
+my %weightingsHash = (
+                      "01A_0801_CTS" =>  0.672849571,
+                      "01A_0809_F1" =>  0.639243628,
+                      "01A_0905_BTS" =>  0.365032135,
+                      "01B_0802_CTS" =>  0.893572736,
+                      "01B_0810_F1" =>  0.822493028,
+                      "01B_0906_BTS" =>  0.178918215,
+                      "01C_0803_CTS" =>  0.71826707,
+                      "01C_0811_F1" =>  0.826762193,
+                      "01C_0907_BTS" =>  0.17182868,
+                      "01D_0804_CTS" =>  0.956316948,
+                      "01D_0812_F1" =>  0.687751098,
+                      "01D_0908_BTS" =>  0.143539041,
+                      "01E_0805_CTS" =>  0.791871027,
+                      "01E_0901_BTS" =>  0.902430467,
+                      "01E_0909_BTS" =>  0.376379171,
+                      "01F_0806_CTS" =>  0.873892117,
+                      "01F_0902_BTS" =>  0.999901051,
+                      "01F_0910_DSN_BTS" =>  0.537961023,
+                      "01G_0807_F1" =>  0.911113481,
+                      "01G_0903_BTS" =>  1,
+                      "01G_0911_DSN_CTS" =>  0.34160701,
+                      "01H_0808_F1" =>  0.751802161,
+                      "01H_0904_BTS" =>  0.733224629,
+                      "02A_0801_F1" =>  0.535103229,
+                      "02A_0905_BTS" =>  0.770791541,
+                      "02B_0802_F1" =>  0.385594786,
+                      "02B_0906_CTS" =>  0.28030874,
+                      "02C_0803_F1" =>  0.616267724,
+                      "02C_0907_CTS" =>  0.669865039,
+                      "02D_0804_F1" =>  0.323149059,
+                      "02D_0908_CTS" =>  0.286276709,
+                      "02E_0805_F1" =>  0.738164993,
+                      "02E_0909_CTS" =>  0.603938742,
+                      "02F_0806_BTS" =>  0.410905105,
+                      "02F_0910_CTS" =>  0.331357946,
+                      "02G_0807_BTS" =>  0.330243715,
+                      "02G_0911_CTS" =>  0.811626187,
+                      "02H_0808_BTS" =>  0.290986203,
+                      "02H_0912_F1" =>  0.454679297,
+                      "03A_0809_BTS" =>  0.337022306,
+                      "03A_0905_CTS" =>  0.294045201,
+                      "03B_0810_BTS" =>  0.292356386,
+                      "03B_0906_F1" =>  0.273154532,
+                      "03C_0811_BTS" =>  0.281317849,
+                      "03C_0907_F1" =>  0.776689419,
+                      "03D_0812_CTS" =>  0.274925807,
+                      "03D_0908_F1" =>  0.339460045,
+                      "03E_0901_CTS" =>  0.700577468,
+                      "03E_0909_F1" =>  0.728416849,
+                      "03F_0902_CTS" =>  0.334234769,
+                      "03F_0910_F1" =>  0.286825336,
+                      "03G_0903_CTS" =>  0.555839238,
+                      "03G_0911_F1" =>  0.724592837,
+                      "03H_0904_CTS" =>  0.625264738,
+                      "03H_0912_BTS" => 0.828501189
+                );
+
+my $outFile1 = $outFilePrefix . ".HSPcounts.txt";
+my $outFile2 = $outFilePrefix . ".aveDepths.txt";
+open(my $outFH1, ">", $outFile1) or die "Couldn't open $outFile1 for writing: $!\n";
+open(my $outFH2, ">", $outFile2) or die "Couldn't open $outFile2 for writing: $!\n";
+
+
+# Print header rows
+print $outFH1 "Sample\taveUnder5Counts\taveOver5Counts\taveOver10Counts\taveOver15Counts\taveOver20Counts\tmax100AveUnder5Counts\tmax100AveOver5Counts\tmax100AveOver10Counts\tmax100AveOver15Counts\tmax100AveOver20Counts\n";
+print $outFH2 "Sample\tAverageTargetDepthAcrossHSP\tmax100AverageTargetDepth\n";
+
+
+my %sampleSummaryHash;
+foreach my $sample (@samples) {
+    my %sampleResultsHash;
+    my $depthFile = $depthDir . $sample . ".merged.cleaned.sorted.markDups.depth";
+    open(my $depthFH, "<", $depthFile) or die "Couldn't open $depthFile for reading: $!\n";
+    
+    # First we'll hash-ify all of the data for a sample.
+    while (my $line = <$depthFH>) {
+        my @fields = split(/\t/, $line);
+        my $target = $fields[0];
+        my $base = $fields[1];
+        my $depth = $fields[2];
+        $sampleResultsHash{$target}{$base} = $depth; # Hash of hashes--with the first key being the name of the target and the second being the position in the target
+    }
+    
+    
+    my $searchIO = Bio::SearchIO->new(-file => $blastFile,
+                                  -format => 'blasttable');
+    while (my $result = $searchIO->next_result()) {
+        my $hitCounter = 0;
+        my $hit = $result->next_hit(); # Just get the best scoring hit (ignore the other ones)
+        my $hsp = $hit->next_hsp(); # Just get the best scoring HSP (ignore the other ones)
+        my $startingBase = $hsp->start('subject');
+        my $endingBase = $hsp->end('subject');
+        my $smallerBase;
+        my $largerBase;
+        if ($startingBase > $endingBase) {
+            $largerBase = $startingBase;
+            $smallerBase = $endingBase;
+        } else {
+            $smallerBase = $startingBase;
+            $largerBase = $endingBase;
+        }
+        my @bases = $smallerBase .. $largerBase; # This generates a sequence of integers between these two endpoints (including the endpoints)
+        if (scalar(@bases) >= 100) {
+            # Calculate average depth over HSP, and check if there's a stretch at least 100bp with average coverage greater than 5, 10, 20
+            my $sumDepthOverHSP = 0;
+            foreach my $base (@bases) {                        
+                if (exists($sampleResultsHash{$hit->name()}{$base})) {
+                    $sumDepthOverHSP += $sampleResultsHash{$hit->name()}{$base};
+                } else {
+                    # Do nothing, i.e. add 0
+                }                        
+            }
+            my $average = $sumDepthOverHSP / scalar(@bases);
+            #print "Average across best HSP for " . $hit->name() . " for individual $sample: $average\n";
+            #print "WEIGHTED Average across best HSP for " . $hit->name() . " for individual $sample: " . $average * $weightingsHash{$sample} . "\n";
+            my $weightedAverage = $average * $weightingsHash{$sample};
+            if ($weightedAverage > 20) {
+                $sampleSummaryHash{$sample}{'aveOver20'}++;
+                $sampleSummaryHash{$sample}{'aveOver15'}++;
+                $sampleSummaryHash{$sample}{'aveOver10'}++;
+                $sampleSummaryHash{$sample}{'aveOver5'}++;
+            } elsif ($weightedAverage > 15) {
+                $sampleSummaryHash{$sample}{'aveOver15'}++;
+                $sampleSummaryHash{$sample}{'aveOver10'}++;
+                $sampleSummaryHash{$sample}{'aveOver5'}++;
+            } elsif ($weightedAverage > 10) {
+                $sampleSummaryHash{$sample}{'aveOver10'}++;
+                $sampleSummaryHash{$sample}{'aveOver5'}++;
+            } elsif ($weightedAverage > 5) {                        
+                $sampleSummaryHash{$sample}{'aveOver5'}++;
+            } else {
+                $sampleSummaryHash{$sample}{'aveUnder5'}++;
+            }
+
+            push(@{$sampleSummaryHash{$sample}{'aveWindowDepth'}}, $weightedAverage);        
+
+            # Need to implement finding the highest coverage for a 100bp window within the best HSP...
+            # Go through each 100bp window of the HSP, and calculate the average depth. If it's higher
+            # than the current depth, then update the max
+            my $max100aveDepth = 0;
+            my $windowDepth = 0;
+            foreach my $baseNum (1 .. (scalar(@bases)-100)) {
+                foreach my $innerBase ($baseNum .. ($baseNum+99)) {
+                    if (exists $sampleResultsHash{$hit->name()}{$innerBase}) {
+                        $windowDepth += $sampleResultsHash{$hit->name()}{$innerBase};
+                    } else {
+                        $windowDepth += 0;
+                    }
+                }
+                my $aveWindowDepth = $windowDepth / 100;
+                if ($aveWindowDepth > $max100aveDepth) {
+                    $max100aveDepth = $aveWindowDepth;
+                }
+            }
+            my $weightedMax100AveDepth = $max100aveDepth * $weightingsHash{$sample};
+            
+            push(@{$sampleSummaryHash{$sample}{'max100AveWindowDepth'}}, $weightedMax100AveDepth);   
+            
+            if ($weightedMax100AveDepth > 20) {
+                $sampleSummaryHash{$sample}{'max100AveOver20'}++;
+                $sampleSummaryHash{$sample}{'max100AveOver15'}++;
+                $sampleSummaryHash{$sample}{'max100AveOver10'}++;
+                $sampleSummaryHash{$sample}{'max100AveOver5'}++;
+            } elsif ($weightedMax100AveDepth > 15) {
+                $sampleSummaryHash{$sample}{'max100AveOver15'}++;
+                $sampleSummaryHash{$sample}{'max100AveOver10'}++;
+                $sampleSummaryHash{$sample}{'max100AveOver5'}++;
+            } elsif ($weightedMax100AveDepth > 10) {
+                $sampleSummaryHash{$sample}{'max100AveOver10'}++;
+                $sampleSummaryHash{$sample}{'max100AveOver5'}++;
+            } elsif ($weightedMax100AveDepth > 5) {                        
+                $sampleSummaryHash{$sample}{'max100AveOver5'}++;
+            } else {
+                $sampleSummaryHash{$sample}{'max100AveUnder5'}++;
+            }
+        } else {
+            #Do nothing--don't process HSPs under 100bp in length
+        }
+    }
+    print $outFH1 "$sample\t$sampleSummaryHash{$sample}{'aveUnder5'}\t$sampleSummaryHash{$sample}{'aveOver5'}\t$sampleSummaryHash{$sample}{'aveOver10'}\t$sampleSummaryHash{$sample}{'aveOver15'}\t$sampleSummaryHash{$sample}{'aveOver20'}\t$sampleSummaryHash{$sample}{'max100AveUnder5'}\t$sampleSummaryHash{$sample}{'max100AveOver5'}\t$sampleSummaryHash{$sample}{'max100AveOver10'}\t$sampleSummaryHash{$sample}{'max100AveOver15'}\t$sampleSummaryHash{$sample}{'max100AveOver20'}\n";
+    print $outFH2 "$sample\t" . sum(@{$sampleSummaryHash{$sample}{'aveWindowDepth'}})/@{$sampleSummaryHash{$sample}{'aveWindowDepth'}} . "\t" . sum(@{$sampleSummaryHash{$sample}{'max100AveWindowDepth'}})/@{$sampleSummaryHash{$sample}{'max100AveWindowDepth'}} . "\n";
+}
+
+
+

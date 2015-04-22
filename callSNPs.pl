63dd506b4921f6ba42ffa6fc8f4ae7ab19fd988f
Evan <evanmelstad@ucla.edu>
2015-04-21 20:16:23 -0700

    Adding SNP calling procedure

--- callSNPs.pl
+++ callSNPs.pl
@@ -1,17 +1,32 @@
 #!/usr/bin/perl
 
 
-system("bwa mem -t 20 ../../ARC/finished_allCTS/RBBHs.CTSonly6iter.chimeraMasked.fasta AllCTS.Ns.un1.fastq AllCTS.Ns.un2.fastq | samtools view -@ 20 -bS - > AllCTS.pe.bam");
-system("bwa mem -t 20 ../../ARC/finished_allCTS/RBBHs.CTSonly6iter.chimeraMasked.fasta AllCTS.Ns.combinedJoinedAndSingles.fastq | samtools view -@ 20 -bS - > AllCTS.se.bam");
+system("bwa mem -t 20 ../ARC/finished_allCTS/RBBHs.CTSonly6iter.chimeraMasked.fasta AllCTS.Ns.un1.fastq AllCTS.Ns.un2.fastq | samtools view -@ 20 -bS - > AllCTS.pe.bam");
+system("bwa mem -t 20 ../ARC/finished_allCTS/RBBHs.CTSonly6iter.chimeraMasked.fasta AllCTS.Ns.combinedJoinedAndSingles.fastq | samtools view -@ 20 -bS - > AllCTS.se.bam");
+
+system("bwa mem -t 20 ../ARC/finished_allCTS/RBBHs.CTSonly6iter.chimeraMasked.fasta AllF1.Ns.un1.fastq AllF1.Ns.un2.fastq | samtools view -@ 20 -bS - > AllF1.pe.bam");
+system("bwa mem -t 20 ../ARC/finished_allCTS/RBBHs.CTSonly6iter.chimeraMasked.fasta AllF1.Ns.combinedJoinedAndSingles.fastq | samtools view -@ 20 -bS - > AllF1.se.bam");
+
+
 
 system("samtools sort -o AllCTS.se.sorted.bam -T blah AllCTS.se.bam");
 system("samtools sort -o AllCTS.pe.sorted.bam -T blah2 AllCTS.pe.bam");
 
+system("samtools sort -o AllF1.se.sorted.bam -T blah3 AllF1.se.bam");
+system("samtools sort -o AllF1.pe.sorted.bam -T blah4 AllF1.pe.bam");
+
 system("samtools merge -@ 20 AllCTS.both.sorted.bam AllCTS.pe.sorted.bam AllCTS.se.sorted.bam");
+system("samtools merge -@ 20 AllF1.both.sorted.bam AllF1.pe.sorted.bam AllF1.se.sorted.bam");
+
 
 system("java -Xmx80g -jar ~/bin/picard-tools-1.125/picard.jar CleanSam I=AllCTS.both.sorted.bam O=AllCTS.cleaned.bam");
 system("java -Xmx80g -jar ~/bin/picard-tools-1.125/picard.jar AddOrReplaceReadGroups I=AllCTS.cleaned.bam O=AllCTS.cleaned.RG.bam SORT_ORDER=coordinate RGPL=illumina RGPU=Test RGLB=Lib1 RGID=CTS RGSM=CTS VALIDATION_STRINGENCY=LENIENT");
 system("java -Xmx40g -jar ~/bin/picard-tools-1.125/picard.jar MarkDuplicates I=AllCTS.cleaned.RG.bam O=AllCTS.cleaned.RG.MD.bam METRICS_FILE=AllCTS.cleaned.RG.MD.metrics MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=250 ASSUME_SORTED=true REMOVE_DUPLICATES=false");
+
+system("java -Xmx80g -jar ~/bin/picard-tools-1.125/picard.jar CleanSam I=AllF1.both.sorted.bam O=AllF1.cleaned.bam");
+system("java -Xmx80g -jar ~/bin/picard-tools-1.125/picard.jar AddOrReplaceReadGroups I=AllF1.cleaned.bam O=AllF1.cleaned.RG.bam SORT_ORDER=coordinate RGPL=illumina RGPU=Test RGLB=Lib1 RGID=F1 RGSM=F1 VALIDATION_STRINGENCY=LENIENT");
+system("java -Xmx40g -jar ~/bin/picard-tools-1.125/picard.jar MarkDuplicates I=AllF1.cleaned.RG.bam O=AllF1.cleaned.RG.MD.bam METRICS_FILE=AllF1.cleaned.RG.MD.metrics MAX_FILE_HANDLES_FOR_READ_ENDS_MAP=250 ASSUME_SORTED=true REMOVE_DUPLICATES=false");
+
 
 system("java -Xmx80g -jar ~/bin/picard-tools-1.125/picard.jar MergeSamFiles SO=coordinate AS=true I=AllCTS.cleaned.RG.MD.bam I=AllF1.cleaned.RG.MD.bam O=CTSandF1.bam");
 system("samtools index CTSandF1.bam");
